b42984a5dfa535aea112d5b60e24bab4498a1a25
Joao Bezerra <vitor.lira98@gmail.com>
2022-10-19 16:45:41 +0000

    feat: create id entity

---
+++ test/unit/core/domain/id.spec.ts
@@ -0,0 +1,22 @@
+import { faker } from '@faker-js/faker'
+import { ID } from '@domain/entities'
+
+describe('ID | Entity', () => {
+  it('should be possible create a instance.', () => {
+    expect(() => new ID()).not.toThrow()
+   })
+
+  it('should throws if invalid value is provided.', () => {
+    const value = 'invalid_uuid'
+    expect(() => new ID(value)).toThrow()
+  })
+
+  it('should not throws if valid value is provided.', () => {
+    const value = faker.datatype.uuid()
+    expect(() => new ID(value)).not.toThrow()
+  })
+
+  it('should be able to get value.', () => {
+    expect(() => new ID().value).not.toThrow()
+  })
+})
