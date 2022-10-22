af97d2f115f1857f514e4b175494b4269579c03e
Joao Bezerra <vitor.lira98@gmail.com>
2022-10-22 19:42:35 +0000

    chore: enable cors

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ config()
 const bootstrap = async (): Promise<void> => {
   const app = await NestFactory.create<NestFastifyApplication>(MainModule, new FastifyAdapter())
   app.useGlobalFilters(new ExceptionHandler())
+  app.enableCors()
   const port = process.env.PORT
   if (!port) throw new Error('ENV:PORT not set.')
   await app.listen(port, '0.0.0.0')
